4cbc67acbc1b3d4047681baf0e47b8ca09504d09
C5356976 <anu.jose01@sap.com>
2023-09-01 15:03:24 +0530

    first commit

---
+++ src/main/java/com/cf/cf_java17/CfJava17Application.java
@@ -0,0 +1,14 @@
+package com.cf.cf_java17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfJava17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfJava17Application.class, args);
+		System.out.println("hello");
+	}
+
+}
---
+++ src/test/java/com/cf/cf_java17/CfJava17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cf.cf_java17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfJava17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
